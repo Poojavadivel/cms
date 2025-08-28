936bc0b93c89dc8afa063a4c07e2d66a0c17a43e
sanjitsriram08 <sanjit@kr-dx.com>
2025-08-28 16:37:27 +0530

    working web

--- Server/web/flutter_bootstrap.js
+++ Server/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2034480750"
+    serviceWorkerVersion: "2996729664"
   }
 });
