56d1cdac2532374fa843a99275fd59a686bbe19f
kishorebalajiit-dev <kishorebalaji.it@gmail.com>
2026-03-06 15:15:50 +0530

    Delete mobile directory

--- mobile/src/provider/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Provider Index - Central export file
- */
-export { AppProviders } from './AppProviders';
-export { AppProvider, useApp } from './AppContext';
-export { ThemeProvider, useTheme, AppColors } from './ThemeContext';
-export { NotificationProvider, useNotification } from './NotificationContext';
-export { LoadingProvider, useLoading } from './LoadingContext';
